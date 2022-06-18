0dd89b8682b420e2b39c690efe3012a3d809fa9a
Paulo Roberto <79487813+Paulodiastst@users.noreply.github.com>
2022-06-18 19:56:30 -0300

    Criando o controller de eventos

---
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -0,0 +1,12 @@
+package ifrn.pi.eventos.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EventosController {
+	
+	public String form() {
+		return "formEvento";
+	}
+
+}
